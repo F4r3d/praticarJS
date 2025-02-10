9b1871bc529bdb0f209fe50436184083c898f424
F4r3d <167693253+F4r3d@users.noreply.github.com>
2025-02-10 15:12:04 -0300

    adicionado cálculo chances Ex 5

--- app.js
+++ app.js
@@ -136,7 +136,8 @@ function calcularValor() {
     let resultado5 = document.querySelector('#resp5');
     let quantidadeTotal = Math.fatorial(quantidade) / (Math.fatorial(6) * Math.fatorial(quantidade - 6))
     let valorTotal = formatarReal(quantidadeTotal * 5);
-    resultado5.innerHTML = ("O valor total da aposta será de " + valorTotal);
+    let chances = Math.ceil(50063860 / quantidadeTotal);
+    resultado5.innerHTML = ("O valor total da aposta será de " + valorTotal + " e a chance de acertar na mega-sena é de 1 em " + chances +".");
 
     const gerar = document.createElement('button');
     gerar.textContent = "Gerar";
